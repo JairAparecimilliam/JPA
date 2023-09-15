1e2e76f8e8593a0a43f3cfc2fe81a89ff8ab4116
Jair <jrabc1991@gmail.com>
2023-09-14 22:44:04 -0300

    create class Programa

--- nivelamJPA/src/dominio/Pessoa.java
+++ nivelamJPA/src/dominio/Pessoa.java
@@ -3,6 +3,7 @@ package dominio;
 import java.io.Serializable;
 
 public class Pessoa implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	private Integer id;
 	private String name;
